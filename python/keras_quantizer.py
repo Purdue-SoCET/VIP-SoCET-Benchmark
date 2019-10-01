64a8d33245d3595d76c225907473fbabe91942a0
jameszampa <Apaches116202>
2019-10-01 19:41:05 -0400

    created KerasQuantizer class

--- python/keras_quantizer.py
+++ python/keras_quantizer.py
@@ -7,9 +7,14 @@ from __future__ import print_function
 import os
 import h5py
 import numpy as np
-import argparse
 import uuid
 import model_pb2
+
+
+"""
+adated from:
+https://github.com/transcranial/keras-js/blob/master/python/encoder.py
+"""
 
 
 def quantize_arr(arr, dtype=np.uint8):
@@ -36,6 +41,7 @@ def quantize_arr(arr, dtype=np.uint8):
 
     return quantized, min_val, max_val
 
+
 def choose_quant_params(min, max, dtype=np.uint8):
     # Function adapted for python from:
     # https://github.com/google/gemmlowp/blob/master/doc/quantization_example.cc
@@ -49,7 +55,7 @@ def choose_quant_params(min, max, dtype=np.uint8):
     if dtype == np.uint8:
         qmin = 0.0
         qmax = 255.0
-    elif dtype == np.uint32:
+    elif dtype == np.int32:
         qmin = -2147483647.0
         qmax = 2147483647.0
     else:
@@ -71,10 +77,10 @@ def choose_quant_params(min, max, dtype=np.uint8):
     # to be able to represent the real value "0" exactly as a quantized value,
     # which is required in multiple places, for example in Im2col with SAME
     # padding).
-    nudged_zero_point = 0
-    if (initial_zero_point < qmin):
+
+    if initial_zero_point < qmin:
         nudged_zero_point = np.uint8(qmin)
-    elif (initial_zero_point > qmax):
+    elif initial_zero_point > qmax:
         nudged_zero_point = np.uint8(qmax)
     else:
         nudged_zero_point = np.uint8(round(initial_zero_point))
@@ -82,7 +88,7 @@ def choose_quant_params(min, max, dtype=np.uint8):
     return scale, nudged_zero_point
 
 
-class Encoder:
+class KerasQuantizer:
     """Encoder class.
     Takes as input a Keras model saved in hdf5 format that includes the model architecture with the weights.
     This is the resulting file from running the command:
@@ -92,29 +98,30 @@ class Encoder:
     See https://keras.io/getting-started/faq/#savingloading-whole-models-architecture-weights-optimizer-state
     """
 
-    def __init__(self, hdf5_model_filepath, name, quantize):
+    def __init__(self, hdf5_model_filepath):
         if not hdf5_model_filepath:
             raise Exception('hdf5_model_filepath must be provided.')
         self.hdf5_model_filepath = hdf5_model_filepath
-        self.name = name
-        self.quantize = quantize
         self.layers = {}
 
         self.create_model()
+        self.quantize()
 
     def create_model(self):
         """Initializes a model from the protobuf definition.
         """
         self.model = model_pb2.Model()
         self.model.id = str(uuid.uuid4())
-        self.model.name = self.name
+        self.model.name = 'MyModel'
 
-    def serialize(self):
-        """serialize method.
+    def quantize(self):
+        """quantize method.
+        Originally was serialize function from encoder.py
         Strategy for extracting the weights is adapted from the
         load_weights_from_hdf5_group method of the Container class:
         see https://github.com/keras-team/keras/blob/master/keras/engine/topology.py#L2505-L2585
         """
+
         hdf5_file = h5py.File(self.hdf5_model_filepath, mode='r')
 
         self.model.keras_version = hdf5_file.attrs['keras_version']
@@ -127,51 +134,10 @@ class Encoder:
             self.layers[layer_name] = {}
             for weight_name in g.attrs['weight_names']:
                 weight_value = g[weight_name].value
-                w = self.model.model_weights.add()
-                w.layer_name = layer_name
-                w.weight_name = weight_name
-                w.shape.extend(list(weight_value.shape))
-                if self.quantize:
-                    w.type = 'uint8'
-                    quantized, min_val, max_val = quantize_arr(weight_value)
-                    self.layers[layer_name][weight_name] = quantized.astype(np.uint8)
-                    w.data = quantized.astype(np.uint8).tobytes()
-                    w.quantize_min = min_val
-                    w.quantize_max = max_val
+                if b'/bias:0' in weight_name:
+                    quant_type = np.int32
                 else:
-                    w.type = 'float32'
-                    w.data = weight_value.astype(np.float32).tobytes()
-
+                    quant_type = np.uint8
+                quantized, min_val, max_val = quantize_arr(weight_value, dtype=quant_type)
+                self.layers[layer_name][weight_name] = quantized.astype(quant_type)
         hdf5_file.close()
-
-    def save(self):
-        """Saves as binary protobuf message
-        """
-        pb_model_filepath = os.path.join(os.path.dirname(self.hdf5_model_filepath),
-                                         '{}.bin'.format(self.name))
-        with open(pb_model_filepath, 'wb') as f:
-            f.write(self.model.SerializeToString())
-        print('Saved to binary file {}'.format(os.path.abspath(pb_model_filepath)))
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('hdf5_model_filepath')
-    parser.add_argument('-n', '--name', type=str, required=False,
-                        help='model name (defaults to filename without extension if not provided)')
-    parser.add_argument('-q', '--quantize', action='store_true', required=False,
-                        help='quantize weights to 8-bit unsigned int')
-    args = parser.parse_args()
-
-    hdf5_model_filepath = args.hdf5_model_filepath
-
-    if args.name is not None:
-        name = args.name
-    else:
-        name = os.path.splitext(os.path.basename(hdf5_model_filepath))[0]
-
-    quantize = args.quantize
-
-    encoder = Encoder(hdf5_model_filepath, name, quantize)
-    encoder.serialize()
-    encoder.save()
